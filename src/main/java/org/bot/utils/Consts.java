3e87c7353cc0ae790c8c309eebd3074cf0f521c4
Taly Tochner <81686032+Taly96@users.noreply.github.com>
2023-11-23 20:48:15 +0200

    Update Consts.java

--- src/main/java/org/bot/utils/Consts.java
+++ src/main/java/org/bot/utils/Consts.java
@@ -3,13 +3,7 @@ package org.bot.utils;
 public class Consts {
     public static final String NO_PROBLEM = "No problem";
 
-    public static final String TEL_USER = "NunChuckBot";
-
-    public static final String TEL_KEY = "6650535648:AAHcOn2lLjm1qzsKxicDna4Nl-tqqYfKGTQ";
-
-    public static final String AZ_KEY = "0310f1fa7b6d4ea787bc32df80429b14";
-
-    public static final String AZ_LOCATION = "germanywestcentral";
+    
 
     public static final String AZ_URL = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0";
 
