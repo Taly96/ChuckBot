c31056c97bc50fb823e461373be4a0e532077167
Taly96 <talicht96@gmail.com>
2023-11-25 21:04:07 +0200

    web scraping

--- src/main/java/org/bot/ChuckBot.java
+++ src/main/java/org/bot/ChuckBot.java
@@ -17,14 +17,11 @@ public class ChuckBot extends TelegramLongPollingBot {
 
     private final TextTranslator translator = new TextTranslator();
 
-
     private final ChuckNorrisJokesScraper scraper = new ChuckNorrisJokesScraper();
 
     private long userID = -1;
 
     private String langCode = null;
-
-    private boolean isLanguageSelected = false;
 
     @Override
     public String getBotUsername() {
@@ -44,7 +41,7 @@ public class ChuckBot extends TelegramLongPollingBot {
         userID = update.getMessage().getFrom().getId();
 
         if(null != msgFromUser && !msgFromUser.isEmpty()){
-            System.out.println(update); // todo: delete
+
             try {
                 handleUpdate(msgFromUser);
             } catch (IOException | TelegramApiException e) {
@@ -56,7 +53,46 @@ public class ChuckBot extends TelegramLongPollingBot {
     private void handleUpdate(String msgFromUser) throws IOException, TelegramApiException {
         String msgToUser = "";
 
+        if(msgFromUser.toLowerCase().startsWith(SET_LANGUAGE)){
+                msgToUser = setLanguage(msgFromUser);
+        }
+        else if (isNumeric(msgFromUser)){
+            if(null != langCode){
+                int jokeNumber = Integer.parseInt(msgFromUser);
+                if(isValidJokeNumber(jokeNumber)){
 
+                    msgToUser = getChuckNorrisJoke(jokeNumber, langCode);
+                }
+                else{
+                    msgToUser = "Please use a number between 1 - 101";
+                }
+            }
+            else{
+                msgToUser = "Please set language first";
+            }
+
+        }else{
+            if(null == langCode){
+                msgToUser = "Please set language first";
+            }
+            else{
+                msgToUser = "please enter a valid number between 1 - 101";
+            }
+        }
+
+        sendText(userID, msgToUser);
+    }
+
+    private boolean isNumeric(String msgFromUser){
+        boolean isNumeric = true;
+
+        try {
+            Integer.parseInt(msgFromUser);
+        } catch (NumberFormatException e) {
+            isNumeric = false;
+        }
+
+        return isNumeric;
     }
 
     private String getChuckNorrisJoke(int jokeNumber, String langCode) throws IOException {
@@ -64,17 +100,9 @@ public class ChuckBot extends TelegramLongPollingBot {
         return translator.Post(scraper.getJokeByNumber(jokeNumber), langCode);
     }
 
-    private boolean isValidJokeNumber(String msgFromUser) {
-        boolean isValidJokeNumber = false;
+    private boolean isValidJokeNumber(int jokeNumber) {
 
-        try {
-            int jokeNumber = Integer.parseInt(msgFromUser);
-            isValidJokeNumber =  jokeNumber >= MIN_JOKE_NUM && jokeNumber <= MAX_JOKE_NUM;
-        } catch (NumberFormatException e) {
-            isValidJokeNumber = false;
-        }
-
-        return isValidJokeNumber;
+        return jokeNumber >= MIN_JOKE_NUM && jokeNumber <= MAX_JOKE_NUM;
     }
 
     private String setLanguage(String msgFromUser) throws IOException {
@@ -84,7 +112,6 @@ public class ChuckBot extends TelegramLongPollingBot {
 
         if(null != langCode) {
             translatedText = translator.Post(NO_PROBLEM, langCode);
-            isLanguageSelected = true;
         }
 
         return translatedText;
